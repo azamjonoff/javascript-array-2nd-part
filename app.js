888d054a72d5f3bff5463a1f8b6903843282e22f
azamjonoff <abdullohazamjonov57gmail.com>
2024-05-04 09:38:43 +0500

    add 10 matters

--- app.js
+++ app.js
@@ -70,7 +70,7 @@
 //   return result;
 // }
 
-// teksari(1, 4, 6);
+// teskari(1, 4, 6);
 
 // function teskari(arr) {
 //   return arr.reverse();
@@ -95,3 +95,85 @@
 // arr(2, 4, 6, 3, 7);
 
 // 5)
+
+// function sonlar(arr) {
+//   let juftlari = [];
+//   let toqlari = [];
+//   for (i = 0; i <= arr.length; i++) {
+//     if (arr[i] % 2 == 0) {
+//       juftlari.push(arr[i]);
+//     } else {
+//       toqlari.push(arr[i]);
+//     }
+//   }
+
+//   return juftlari.concat(toqlari.sort());
+// }
+
+// let arr = [4, 5, 7, 8, 6, 9];
+// console.log(sonlar(arr));
+
+// 7)
+
+// function elementSeries(arr) {
+//   let arrBracket = [];
+
+//   for (let i = arr.length - 1; i >= 0; i -= 2) {
+//     arrBracket.push(arr[i]);
+//   }
+
+//   return arrBracket;
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// console.log(elementSeries(arr));
+
+// 8)
+
+// function jufttoq(arr) {
+//   let arrBracket = [];
+//   let juft = [];
+//   let toq = [];
+
+//   for (i = 0; i <= arr.length; i++) {
+//     if (arr[i] % 2 == 0) {
+//       juft.push(arr[i]);
+//     } else {
+//       toq.push(arr[i]);
+//     }
+//   }
+
+//   return juft.concat(toq);
+// }
+
+// let arr = [1, 5, 8, 4, 9];
+// console.log(jufttoq(arr));
+
+// 9)
+
+// function jufttoq(arr) {
+//   let arrBracket = [];
+//   let juft = [];
+//   let toq = [];
+
+//   for (i = 0; i <= arr.length; i++) {
+//     if (arr[i] % 2 == 0) {
+//       juft.push(arr[i]);
+//     } else {
+//       toq.push(arr[i]);
+//     }
+//   }
+
+//   return toq.sort().concat(juft.reverse());
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(jufttoq(arr));
+
+// 10)
+
+// 11)
+
+function rangeOutSum(arr) {
+    
+}
